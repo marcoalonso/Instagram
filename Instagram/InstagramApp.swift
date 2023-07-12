da3f18741ccb4235a868db018b433251978d97a9
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-07-12 15:51:18 -0600

    Getting Started

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Marco Alonso Rodriguez on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
