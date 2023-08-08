7a66b754174ad9388c9a5804be6cf38af9df8981
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-08-08 10:18:01 -0600

    Module 19 Profile Action Button View

--- Instagram/Core/Profile/ProfileHeaderView.swift
+++ Instagram/Core/Profile/ProfileHeaderView.swift
@@ -54,15 +54,21 @@ struct ProfileHeaderView: View {
             
             // action button
             Button {
-                
+                if user.isCurrentUser {
+                    print("Show edit profile")
+                } else {
+                    print("Follow user ...")
+                }
             } label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
-                    .foregroundColor(.black)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 1)
+                        RoundedRectangle(cornerRadius: 6).stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1)
                     )
             }
 
