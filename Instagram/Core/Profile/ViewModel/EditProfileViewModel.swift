c3f5a6d06a61eb789989bf17d5468f24cd6a012a
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-08-15 10:32:37 -0600

    Module 22 Upload Profile Image

--- Instagram/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Instagram/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -21,6 +21,8 @@ class EditProfileViewModel: ObservableObject {
     @Published var fullname = ""
     @Published var bio = ""
     
+    private var uiImage: UIImage?
+    
     init(user: User){
         self.user = user
     }
@@ -29,12 +31,18 @@ class EditProfileViewModel: ObservableObject {
         guard let item = item else { return }
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
         self.profileImage = Image(uiImage: uiImage)
     }
     
     func updateUserData() async throws {
         
         var data = [String: Any]()
+        
+        if let uiImage = uiImage {
+            let imageUrl = try? await ImageUploader.uploadImage(image: uiImage)
+            data["profileImageUrl"] = imageUrl
+        }
         
         if !fullname.isEmpty && user.fullname != fullname {
             data["fullname"] = fullname
